266108f4d151c9c978310d02ca6b68ceadcaeff2
Victor Nunes <victornunes470@gmail.com>
2019-04-16 18:04:45 -0300

    Envio de Email Completo(Necessita ter um servidor de Email)

--- send.php
+++ send.php
@@ -1,7 +1,18 @@
 <?php 
+	//Necessita ter um servidor de email
 
 	require ('verifica.php');
 
 	if(verifica($_POST['nome']) && verifica($_POST['email']) && verifica($_POST['mensagem'])){
-		echo 'ok';
+		$nome = $_POST['nome'];
+		$email = $_POST['email'];
+		$texto = $_POST['mensagem'];
+
+		$para = "victornunes470@gmail.com";
+		$assunto = "Contato pelo Formulário do Site";
+		$corpo = "Nome: ".$nome." - Email: ".$email." - Mensagem: ".$texto;
+		$cabecalho = "From: victornunes470@gmail.com"."\r\n"."Reply-To: ".$email."\r\n"."X-Mailer: PHP/".phpversion();
+		mail($para, $assunto, $corpo, $cabecalho);
+		echo "Enviado com Sucesso";
+		exit;
 	}
